46771b060a516586c431ca6cf9c0265c5fe85570
ltminh25 <tuanminh30042005@gmail.com>
2026-05-09 12:15:26 +0700

    fix 7

--- src/features/auth/hooks/useLogin.ts
+++ src/features/auth/hooks/useLogin.ts
@@ -1,6 +1,6 @@
-import { useEffect, useState, type ChangeEvent, type FormEvent } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../../app/store/hooks";
+import { useAppDispatch } from "../../../app/store/hooks";
 import { authApi } from "../api/authApi";
 import type { LoginPayload } from "../api/authDTO";
 import { logout, setCredential } from "../model/authSlice";
